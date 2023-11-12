1968f48000e35232185eb32e5be4d27413fbc69b
Faysal Ahmed <faysalahmed.fa538@gmail.com>
2023-11-12 13:34:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DSSS_homework_2',
+    version='1.0.0',
+    packages=find_packages(),  # Include all packages in the project
+
+    # Project metadata
+    author='FA-538',
+    author_email='youremail@example.com',
+    description='Description of your project',
+    url='https://github.com/FA-538/DSSS_homework_2.git',
+    
+    # Dependencies
+    install_requires=[
+
+        
+        # Add other dependencies here
+    ],
+
+    # Other configurations
+    classifiers=[
+        'Programming Language :: Python :: 3.12.0',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
