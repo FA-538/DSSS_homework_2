1affa82e7cd42fd3d87739dbfb703e0ff1b3afb9
Faysal Ahmed <faysalahmed.fa538@gmail.com>
2023-11-12 13:32:09 +0100

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,70 @@
 import random
 
 
-def function_A(min, max):
+def function_random_value(min_val, max_val):
     """
-    Random integer.
+    This function will generate a random value from the given minimum and maximum value.
     """
-    return random.randint(min, max)
+    return random.randint(min_val, max_val)
 
 
-def function_B():
-    return random.choice(['+', '-', '*'])
+def function_operator():
+    """
+    This function generates a random operator from the given options.
+    """
+    return random.choice(['+', '-', '*', '/'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def function_calculate(number1, number2, operator):
+    problem = f"{number1} {operator} {number2}"
+    if operator == '+':
+        ans = number1 + number2
+    elif operator == '-':
+        ans = number1 - number2
+    elif operator == '*':
+        ans = number1 * number2
+    else:
+        if number2 == 0 and operator == '/':
+            print("Error: Division By Zero is not allowed.")
+            return None
+        ans = number1 / number2
+    return problem, ans
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    total_question = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_question):
+        n1 = function_random_value(1, 10)
+        n2 = function_random_value(1, 5)  # changed the max value to an integer
+        o = function_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, answer = function_calculate(n1, n2, o)
+        if answer is None:
+            continue  # Skip this question if there was a division by zero
 
-        if useranswer == ANSWER:
+        print(f"\nQuestion: {problem}")
+        user_answer = input("Your answer: ")
+        try:
+            user_answer = float(user_answer)  # Allowing float input for division
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    
+    final_score =score/total_question 
+    print(f"\nGame over! Your score is: {final_score}")
+
 
 if __name__ == "__main__":
     math_quiz()
